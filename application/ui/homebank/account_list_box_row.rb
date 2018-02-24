b165bd9f20e80a0482cca29ec80248018850a0ef
sabina hofmann <sabinahofmann@gmx.de>
2018-02-24 15:18:17 +0100

    show previous data

--- application/ui/homebank/account_list_box_row.rb
+++ application/ui/homebank/account_list_box_row.rb
@@ -5,11 +5,44 @@ module Homebank
     class << self
       def init
         set_template resource: '/de/hofmann/homebank-gtk/ui/account_list_box_row.ui'
+
+        bind_template_child 'account_details_button'
+        bind_template_child 'account_title_label'
+        bind_template_child 'account_details_revealer'
+        bind_template_child 'account_notes_text_view'
+        bind_template_child 'delete_button'
+        bind_template_child 'edit_button'
       end
     end
 
     def initialize(item)
       super()
+
+      account_title_label.text = item.bank_name || ""
+      account_notes_text_view.buffer.text = item.notes
+
+      account_details_button.signal_connect 'clicked' do
+        account_details_revealer.set_reveal_child !account_details_revealer.reveal_child?
+      end
+
+      delete_button.signal_connect 'clicked' do
+        item.delete!
+
+        #Locate the application window
+        application_window = application.windows.find { |w| w.is_a? Homebank::ApplicationWindow }
+        application_window.load_account
+      end
+
+      edit_button.signal_connect 'clicked' do
+        new_account_window = Homebank::NewAccountWindow.new(application, item)
+        new_account_window.present
+      end
+    end
+
+    def application
+      parent = self.parent
+      parent = parent.parent while !parent.is_a? Gtk::Dialog
+      parent.application
     end
   end
 end
