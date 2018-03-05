e261be803b7a03d3cd5b7dee5109f9c4c7fd5059
sabina hofmann <sabinahofmann@gmx.de>
2018-03-05 06:15:43 +0100

    init file choose button method

--- application/ui/homebank/cvs_convert_window.rb
+++ application/ui/homebank/cvs_convert_window.rb
@@ -6,6 +6,7 @@ module Homebank
       def init
         set_template resource: '/de/hofmann/homebank-gtk/ui/cvs_convert_window.ui'
 
+        bind_template_child 'file_chooser_button'
         bind_template_child 'convert_button'
         bind_template_child 'cancel_button'
       end
@@ -13,6 +14,26 @@ module Homebank
 
     def initialize(application, item)
       super application: application
+
+      file_chooser_button = Gtk::FileChooserButton.new(
+        "Select a file", Gtk::FileChooserAction::SELECT_FOLDER)
+      file_chooser_button.current_folder = GLib.home_dir
+
+      # filter only cvs
+      filter = Gtk::FileFilter.new
+      filter.name = "CVS Files"
+      filter.add_pattern('*.cvs')
+      file_chooser_button.add_filter(filter)
+
+      # select file
+      file_chooser_button.signal_connect 'selection_changed'  do |w|
+        file_changed(choo_file_btt, label)
+      end
+
+      # convert cvs
+      convert_button.signal_connect 'clicked' do
+        puts "convert button #1 clicked"
+      end
 
       # cancel
       cancel_button.signal_connect 'clicked' do |button|
@@ -24,6 +45,13 @@ module Homebank
       parent = self.parent
       parent = parent.parent while !parent.is_a? Gtk::Dialog
       parent.application
+    end
+
+    # file method
+    def file_changed(choo_file, label)
+      file = choo_file.filename
+      file = "" if file == nil
+      label.text = file
     end
   end
 end
