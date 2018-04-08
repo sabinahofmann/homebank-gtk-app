bfa76a5b9c939def7fa8e9d0004a323d810a48fd
sabina hofmann <sabinahofmann@gmx.de>
2018-04-08 22:46:02 +0200

    add contetns dialog

--- application/ui/homebank/about_dialog.rb
+++ application/ui/homebank/about_dialog.rb
@@ -5,11 +5,24 @@ module Homebank
     class << self
       def init
         set_template resource: '/de/hofmann/homebank-gtk/ui/about_dialog.ui'
+        bind_template_child 'button_box'
       end
     end
 
-    def initialize(account)
-      super()
+    def initialize(application)
+      super application: application
+
+      button_box.each_with_index do |but, i|
+        case i
+        when 0 then but.label = 'Credits'
+        when 1 then but.label = 'License'
+        when 2 then but.label = 'Close'
+        end
+      end
+
+      if run == Gtk::ResponseType::DELETE_EVENT
+       close
+      end
     end
   end
 end
